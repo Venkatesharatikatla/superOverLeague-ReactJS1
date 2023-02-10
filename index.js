117435befe9c3a2f76444f74fc12abfe5a63d8bc
Venkatesharatikatla <venkatesharatikatla8@gmail.com>
2023-02-10 10:44:34 +0000

    superOverLeague-cd-1-reactjs

---
+++ index.js
@@ -0,0 +1,19 @@
+const element = (
+  <div className="container">
+    <div>
+      <h1 className="heading">Super Over League</h1>
+    </div>
+    <div className="img-conatiner">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+        className="img"
+      />
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+        className="img"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
